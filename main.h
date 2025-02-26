c3a991ae112a7d2a1f55a09a68a04fa769a1ca9d
Pham Nguyen Quoc Hung <hunqp.repo@gmail.com>
2025-02-26 23:06:16 +0700

    Update all

--- main.h
+++ main.h
@@ -1,34 +1,42 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _APP_H_
+#define _APP_H_
 
 #include <stdio.h>
-#include <bits/stdc++.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #include <mosquitto.h>
 #include <mosquittopp.h>
 
-#define configSIGNALINGSERVER       (char*)"broker.emqx.io"
-#define configSIGNALINGPORT         1883
+#define configSIGNALINGSERVER   (char*)"broker.emqx.io"
+#define configSIGNALINGPORT     1883
 
-#define configSTUNSERVER            (char*)"stun.l.google.com"
-#define configSTUNPORT              19302
+#define configSTUNSERVER        (char*)"stun.l.google.com"
+#define configSTUNPORT          19302
 
-#define configSIGNALING_TOPIC_REQ   "SIGNALING_REQ"
-#define configSIGNALING_TOPIC_RES   "SIGNALING_RES"
+#define configPUNCHING_CMD      (char*)"UDP_HOLE_PUNCHING"
+
+enum {
+    STATE_WAITING,
+    STATE_GATHERING,
+    STATE_CONNECTED,
+};
 
 typedef struct {
     int port;
     int state;
-    std::string uuid;
-    std::string PriIP;
-    std::string PubIP;
+    char PriIP[16];
+    char PubIP[16];
+    char name[32];
 } PeerCandidate;
 
 #define LOGP(fmt, ...)  printf(fmt, ##__VA_ARGS__)
 #define LOGD(fmt, ...)  printf("\x1B[34m""\e[3m" fmt "\x1B[0m", ##__VA_ARGS__)
 #define LOGE(fmt, ...)  printf("\x1B[31m" fmt "\x1B[0m", ##__VA_ARGS__)
 
-extern const struct STUNServer ourSTUN;
-
-
-#endif /* MAIN_H */
+#endif /* _APP_H_ */
